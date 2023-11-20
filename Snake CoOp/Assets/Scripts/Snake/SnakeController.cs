7822456f9af713d953d407fb1e38f3379ebe9769
Roopesh Verma <roopeshv.30@gmail.com>
2023-11-20 10:08:00 +0530

    Setup Snake controller

---
+++ Snake CoOp/Assets/Scripts/Snake/SnakeController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnakeCoOp.Snake
+{
+    public class SnakeController : MonoBehaviour
+    {
+        #region --------- Serialized Variables ---------
+        [SerializeField] private float snakeSpeed;
+        #endregion ------------------
+
+        #region --------- Private Variables ---------
+        private float horizontalAxis;
+        private float verticalAxis;
+        #endregion ------------------
+
+        #region --------- Public Variables ---------
+        #endregion ------------------
+
+        #region --------- Monobehavior Methods ---------
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+        #endregion ------------------
+
+        #region --------- Private Methods ---------
+        #endregion ------------------
+
+        #region --------- Public Methods ---------
+        #endregion ------------------
+    }
+}

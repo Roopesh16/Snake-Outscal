e61a61138e781db63a2bd8edad190928f427d2da
Roopesh Verma <roopeshv.30@gmail.com>
2023-11-21 15:59:42 +0530

    Add Game Manager

---
+++ Snake CoOp/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public enum State
+{
+    NONE,
+    ALIVE,
+    DEAD
+}
+
+public class GameManager : MonoBehaviour
+{
+    #region --------- Public Variables ---------
+    public static GameManager Instance = null;
+    #endregion ------------------
+
+    #region --------- Private Variables ---------
+    private State state = State.NONE;
+    #endregion ------------------
+
+    #region --------- Monobehavior Methods ---------
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }
+    #endregion ------------------
+
+    #region --------- Public Methods ---------
+    public void SetState(State state)
+    {
+        this.state = state;
+    }
+
+    public State GetState()
+    {
+        return state;
+    }
+    #endregion ------------------
+
+
+}

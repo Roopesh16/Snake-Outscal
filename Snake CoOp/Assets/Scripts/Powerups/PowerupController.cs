6744761af99d9b992b6f9e93539df6378cf3ead4
Roopesh Verma <roopeshv.30@gmail.com>
2023-11-21 20:02:50 +0530

    Randomly generate powerup at different times

--- Snake CoOp/Assets/Scripts/Powerups/PowerupController.cs
+++ Snake CoOp/Assets/Scripts/Powerups/PowerupController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using SnakeCoOp.Grid;
 using SnakeCoOp.Snake;
+using SnakeCoOp.Food;
 
 namespace SnakeCoOp.Powerup
 {
@@ -8,12 +9,13 @@ namespace SnakeCoOp.Powerup
     {
         #region --------- Serialized Variables ---------
         [SerializeField] private GridController gridController;
-        [SerializeField] private GameObject[] powerUpPrefabs;
         [SerializeField] private SnakeController snake;
+        [SerializeField] private FoodController food;
+        [SerializeField] private GameObject[] powerUpPrefabs;
         #endregion ------------------
 
         #region --------- Private Variables ---------
-        private GameObject powerUp;
+        private GameObject powerUp = null;
         private Vector2Int powerUpPosition;
         private int powerupIndex = 0;
         private float powerupTimer = 0f;
@@ -24,11 +26,6 @@ namespace SnakeCoOp.Powerup
         #endregion ------------------
 
         #region --------- Monobehavior Methods ---------
-        void Start()
-        {
-            SpawnPowerup();
-        }
-
         private void Update()
         {
             if (GameManager.Instance.GetState() == State.ALIVE)
@@ -36,12 +33,11 @@ namespace SnakeCoOp.Powerup
                 powerupTimer += Time.deltaTime;
                 if (powerUp != null)
                 {
+                    print(powerupTimer);
                     if (powerupTimer >= maxPowerupTimer)
                     {
                         powerupTimer -= maxPowerupTimer;
                         Destroy(powerUp);
-                        SpawnPowerup();
-
                     }
                     else
                     {
@@ -49,8 +45,6 @@ namespace SnakeCoOp.Powerup
                         {
                             powerupTimer -= maxPowerupTimer;
                             Destroy(powerUp);
-                            snake.IncreaseSnakeSize();
-                            SpawnPowerup();
                         }
                     }
                 }
@@ -63,18 +57,39 @@ namespace SnakeCoOp.Powerup
         #endregion ------------------
 
         #region --------- Public Methods ---------
-        #endregion ------------------
-
-        #region --------- Private Methods ---------
         public void SpawnPowerup()
         {
+            if (powerUp != null)
+            {
+                return;
+            }
+
+            powerUp = GetRandomPowerUp();
+
             do
             {
                 powerUpPosition = new Vector2Int(Random.Range(0, gridController.GetGridWidth()), Random.Range(0, gridController.GetGridHeight()));
-            } while (snake.GetFullSnakeSize().IndexOf(powerUpPosition) != -1);
+            } while ((snake.GetFullSnakeSize().IndexOf(powerUpPosition) != -1) && powerUpPosition != food.GetFoodPosition());
 
             powerUp = Instantiate(powerUpPrefabs[powerupIndex]);
             powerUp.transform.position = new Vector2(powerUpPosition.x, powerUpPosition.y);
+            powerupTimer = 0f;
+        }
+        #endregion ------------------
+
+        #region --------- Private Methods ---------
+
+        private GameObject GetRandomPowerUp()
+        {
+            int randVal = Random.Range(0, 100);
+
+            if (randVal <= 20)
+            {
+                powerupIndex = Random.Range(0, 3);
+                return powerUpPrefabs[powerupIndex];
+            }
+
+            return null;
         }
         #endregion ------------------
     }
